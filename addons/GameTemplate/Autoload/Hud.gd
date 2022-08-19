02830e00dd4ac95b5cc90a1ecaecae38020908dc
agustinoli <aolivaarias@gmail.com>
2022-08-19 02:39:49 -0300

    chore: added hud icons

--- addons/GameTemplate/Autoload/Hud.gd
+++ addons/GameTemplate/Autoload/Hud.gd
@@ -4,6 +4,7 @@ onready var mineral: = $CanvasLayer/GUI/MarginContainer/VBoxContainer/Top/Minera
 onready var power: = $CanvasLayer/GUI/MarginContainer/VBoxContainer/Top/PowerNum
 onready var food: = $CanvasLayer/GUI/MarginContainer/VBoxContainer/Top/FoodNum
 onready var water: = $CanvasLayer/GUI/MarginContainer/VBoxContainer/Top/WaterNum
+onready var battery: = $CanvasLayer/GUI/MarginContainer/VBoxContainer/Top/Barrita/BarritaAdentro
 onready var gui: = $CanvasLayer/GUI
 onready var res_labels = [power,mineral,food,water]
 
@@ -24,4 +25,10 @@ func set_values(new_res):
 	mineral.set_text(str(new_res[1]))
 	food.set_text(str(new_res[2]))
 	water.set_text(str(new_res[3]))
+	
+
+func set_battery(percentage):
+	pass
+#	mmm parece qe no es rec_clip_content
+#	battery.rect_clip_content(battery.rect_size()/2)
 	
